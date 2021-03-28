3a86e343aaf23eb958661d4240fa4b9d6db1f2b5
Junhao Zhang <junhaodev@gmail.com>
2021-03-28 13:47:05 -0500

    Updated docs

--- lib/repldb.rb
+++ lib/repldb.rb
@@ -4,19 +4,23 @@ require 'json'
 require 'net/http'
 
 class Client
-  # Change `url` to point to a different path.
+  # Change <tt>url</tt> to point to a different path.
+  # @param url [String, nil]
   def initialize(url = ENV['REPLIT_DB_URL'])
     @url = url
   end
 
-  # Sets `key` to `val`.
+  # Sets <tt>key</tt> to <tt>val</tt>.
+  # @param key [String] the key to set.
+  # @param val [Any] the value.
   def set(key, val)
     post_url(@url.to_s, serialize(key) => JSON.generate(val))
   end
 
-  # Gets `key`.
-  #
-  # If it doesn't exist, `default` will be used.
+  # Gets <tt>key</tt>.
+  # If it doesn't exist, <tt>default<tt> will be used.
+  # @param key [String] the key to get
+  # @param default [Any] the default value if the key doesn't exist.
   def get(key, default = nil)
     val = get_url("#{@url}/#{serialize(key)}")
     if val.body == ''
@@ -31,13 +35,14 @@ class Client
   end
 
   # Get all the keys as an array.
-  #
-  # `prefix` can be used to get all keys with a prefix.
+  # <tt>prefix</tt> can be used to get all keys with a prefix.
+  # @param prefix [String, nil] Get all keys with <tt>prefix</tt> at the start.
   def keys(prefix = '')
     get_url("#{@url}?prefix=#{prefix}").body.split("\n").map { |k| deserialize(k) }
   end
 
-  # Deletes `key`.
+  # Deletes <tt>key</tt>.
+  # @param key [String]
   def del(key)
     uri = URI.parse("#{@url}/#{serialize(key)}")
     req = Net::HTTP::Delete.new(uri.path)
@@ -52,9 +57,9 @@ class Client
     keys.each { |k| del(k) }
   end
 
-  # This will append `hash`.
-  #
+  # This will append <tt>hash</tt>.
   # **This will NOT replace existing keys!**
+  # @param hash [Hash]
   def set_hash(hash)
     if hash.is_a? Hash
       hash.each { |k, v| set(k, v) }
@@ -71,9 +76,10 @@ class Client
     o
   end
 
-  # Check if `key` exists.
+  # Check if <tt>key</tt> exists.
+  # @param key [String]
   def exists?(key)
-    not get(key).nil?
+    !get(key).nil?
   end
 
   # internals
